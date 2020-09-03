3e980d5e3b0c0a6b8032a83f6d10ac8f53fba4d3
Cristian Rodriguez Canto <cristian.rodriguez@maptek.cl>
2020-09-02 20:23:29 -0400

    stats

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,6 @@
 import * as THREE from '/build/three.module.js'
 import { OrbitControls } from '/jsm/controls/OrbitControls'
+import Stats from '/jsm/libs/stats.module'
 
 const scene: THREE.Scene = new THREE.Scene()
 
@@ -10,8 +11,7 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
-controls.addEventListener('change', render) //this line is unnecessary if you are re-rendering within the animation loop 
-
+controls.addEventListener('change', render) 
 
 const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
 const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
@@ -29,17 +29,24 @@ function onWindowResize() {
     render()
 }
 
-// var animate = function () {
-//     requestAnimationFrame(animate)
+const stats = Stats()
+document.body.appendChild(stats.dom)
 
-//     cube.rotation.x += 0.01;
-//     cube.rotation.y += 0.01;
+var animate = function () {
+    requestAnimationFrame(animate)
 
-//     renderer.render(scene, camera)
-// };
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+
+    render()
+
+    stats.update()
+};
 
 function render() {
+    stats.begin()
     renderer.render(scene, camera)
+    stats.end()
 }
 render()
 //animate();
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,6 @@
-import express from "express"
-import path from "path"
 import http from "http"
+import path from "path"
+import express from "express"
 
 const port: number = 3000
 
@@ -14,13 +14,13 @@ class App {
         app.use(express.static(path.join(__dirname, '../client')))
         app.use('/build/three.module.js', express.static(path.join(__dirname, '../../node_modules/three/build/three.module.js')))
         app.use('/jsm/controls/OrbitControls', express.static(path.join(__dirname, '../../node_modules/three/examples/jsm/controls/OrbitControls.js')))
-
+        app.use('/jsm/libs/stats.module', express.static(path.join(__dirname, '../../node_modules/three/examples/jsm/libs/stats.module.js')))
         this.server = new http.Server(app);
     }
 
     public Start() {
         this.server.listen(this.port, () => {
-            console.log( `Server listening ${this.port}.` )
+            console.log(`Server listening on Port ${this.port}.`)
         })
     }
 }
