8a250d20f07755852f922e50a7e0845560a64c83
Cristian Rodriguez Canto <cristian.rodriguez@maptek.cl>
2020-09-02 20:20:09 -0400

    animation

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,4 +1,5 @@
 import * as THREE from '/build/three.module.js'
+import { OrbitControls } from '/jsm/controls/OrbitControls'
 
 const scene: THREE.Scene = new THREE.Scene()
 
@@ -8,24 +9,37 @@ const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.addEventListener('change', render) //this line is unnecessary if you are re-rendering within the animation loop 
+
+
 const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
 const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
 
 const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-camera.position.z = 6
+camera.position.z = 2
 
-var animate = function () {
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
 
-    requestAnimationFrame(animate)
+// var animate = function () {
+//     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    cube.rotation.z += 0.01;
+//     cube.rotation.x += 0.01;
+//     cube.rotation.y += 0.01;
 
+//     renderer.render(scene, camera)
+// };
+
+function render() {
     renderer.render(scene, camera)
-
-}; 
-
-animate(); 
+}
+render()
+//animate();
